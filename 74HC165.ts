00c7274aaf0988b53c19d288270f93b4d0250fd3
lxlongKunkka <110076790@qq.com>
2022-04-14 00:22:33 +0800

    更新 74HC165.ts

--- 74HC165.ts
+++ 74HC165.ts
@@ -2,7 +2,7 @@
 
 let SR_CLK = DigitalPin.P16;
 let INSR0_DATA = DigitalPin.P12;        //data
-//let INSR_LATCH = DigitalPin.P9;         //PL
+let INSR_LATCH = DigitalPin.P9;         //PL
 
 enum KEY {
     UP = 0,
@@ -18,20 +18,26 @@ enum KEY {
 namespace SimpleShieldKey {
     function Read74HC165(): number {
         let val = 0;
-        Servo.setPwm(9, 0, 0);
-        //pins.digitalWritePin(INSR_LATCH, 0);    //scan
-        Servo.setPwm(9, 0, 4095);
-        //pins.digitalWritePin(INSR_LATCH, 1);
+
+        pins.digitalWritePin(INSR_LATCH, 0);    //scan
+        control.waitMicros(1000);
+        pins.digitalWritePin(INSR_LATCH, 1);
         let i = 0;
         for (i = 0; i < 8; i++) {
+            //basic.showNumber(i);
+            //control.waitMicros(1000000);
             val = val << 1;
-            pins.digitalWritePin(SR_CLK, 0);
-            Servo.SetLED(2, false);
-            pins.digitalWritePin(SR_CLK, 1);
-            Servo.SetLED(2, true);
             let tmp = pins.digitalReadPin(INSR0_DATA);
+            //basic.showNumber(tmp);
             val |= tmp;
+            //control.waitMicros(1000);
+            pins.digitalWritePin(SR_CLK, 0);
+            control.waitMicros(1000);
+            pins.digitalWritePin(SR_CLK, 1);
+            control.waitMicros(1000);
         }
+        //basic.showNumber(val);
+        //control.waitMicros(2000000);
         return val;
     }
 
